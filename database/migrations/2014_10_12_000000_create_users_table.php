b31b47448d159dacc0099105f4b80ebfa602855b
Jovi <837717107@qq.com>
2019-03-20 16:59:36 +0800

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id')->comment('自增长 id');
+            $table->string('name')->comment('用户名称');
+            $table->string('email')->unique()->comment('用户邮箱,unique指定该字段的值为唯一值');
+            $table->timestamp('email_verified_at')->nullable()->comment('空的话意味着还未验证邮箱');
+            $table->string('password')->comment('用户密码');
+            $table->rememberToken()->comment('保存『记住我』的相关信息');
             $table->timestamps();
         });
     }
