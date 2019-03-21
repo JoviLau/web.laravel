8f7496a030eebfadc033fdb1a1c6940e09252598
Jovi <837717107@qq.com>
2019-03-21 10:26:37 +0800

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,11 +20,10 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        return $this->validate($request,[
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
     }
 }
